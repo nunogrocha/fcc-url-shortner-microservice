d0cef1e7e4e898d065e0ba905e43dcf31af01fff
Nuno Rocha <nunogcrocha@outlook.com>
2016-06-04 20:39:53 +0100

    0.1

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,52 @@ var router = express.Router();
 var Link = require('./../models/link');
 var mongoose = require('mongoose');
 
-function isValidUrl(t)
+function isValidUrl(str)
 {
-  return t.match(/^(http|https|ftp):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i)
+  var pattern = /([^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
+  return pattern.test(str);
 }
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'FreeCodeCamp Challenge: URL Shortener Microservice' });
-});
+function getRnd() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 4; i++ ) 
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
 
-router.get('/new/:url', function(req, res) {
-  if (isValidUrl(req.params.url)) {
-    /*
+router.get('/new/:url*', function(req, res) {
+  var url = req.originalUrl.replace(/\/new\//, "")
+  if (isValidUrl(url) && url.startsWith('http')) {
     var newLink = new Link({
-      link: req.body.email,
-      shortened: req.body.password
+      link: url,
+      shortened: getRnd()
     });
-    */
-    console.log(req.params.url)
+    newLink.save(function(err) {
+      if (err) throw err;
+    });
+    res.send({ "original_url":url, "short_url":"https://fcc-url-short-microservice-nr.herokuapp.com/" + newLink.shortened });
   } else {
-    res.send({ "error": "invalid url" });
+    res.send({ "error": "invalid url. must start with http or https." });
   }
 });
 
 router.get('/:short', function(req, res) {
-  
-  
+  if (req.params.short != 'favicon.ico') {    
+    Link.findOne({ 'shortened': req.params.short }, function (err, link) {
+      if (err) return handleError(err);
+      if (link) {
+        res.redirect(link.link);
+      } else {
+        res.render('index', { title: 'FreeCodeCamp Challenge: URL Shortener Microservice' });
+      }
+    }) 
+  } 
+});
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'FreeCodeCamp Challenge: URL Shortener Microservice' });
+
 });
 
 module.exports = router;
